fed11a53c79896b41ecd8f0cc40ec3bfadc204ba
Nana Chuto <mail@nanachuto.com>
2023-02-19 16:05:27 +0100

    Iteration 3: Route to movies.hbs done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const Movie = require("../models/Movie.model.js");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+/* GET movies */
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((allTheMoviesFromDB) => {
+      res.render("movies.hbs", { movies: allTheMoviesFromDB });
+    })
+    .catch((error) => {
+      console.log("Error while getting the movies from the DB: ", error);
+      next(error);
+    });
+});
 
 module.exports = router;
