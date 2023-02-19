e1877469816015304a05b068e41477ad2fa25246
Nana Chuto <mail@nanachuto.com>
2023-02-20 00:09:19 +0100

    Iteration 4: done

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,25 @@ router.get("/movies", (req, res, next) => {
   Movie.find()
     .then((allTheMoviesFromDB) => {
       res.render("movies.hbs", { movies: allTheMoviesFromDB });
-      console.log(allTheMoviesFromDB);
     })
     .catch((error) => {
       console.log("Error while getting the movies from the DB: ", error);
+      next(error);
+    });
+});
+
+/* GET movie details */
+
+router.get("/movies/:movieId", (req, res, next) => {
+  const { movieId } = req.params;
+
+  Movie.findById(movieId)
+    .then((movie) => {
+      console.log(movie);
+      res.render("movie-details", { movie: movie });
+    })
+    .catch((error) => {
+      console.log("Error while retrieving movie details: ", error);
       next(error);
     });
 });
